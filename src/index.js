7e525ec96eeb05595c20dc9dfb1514e7d62e92f4
Rudra Arora <aroraru@tcd.ie>
2023-12-21 00:25:50 +0000

    add: wasmer worker starter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+async function handler(request) {
+  const out = JSON.stringify({
+    success: true,
+    package: "wasmer/js-worker-starter",
+  });
+  return new Response(out, {
+    headers: { "content-type": "application/json" },
+  });
+}
+
+addEventListener("fetch", handler);
